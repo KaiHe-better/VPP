907a76e4cd6ce6912ae52214fca8a65302b7ec5e
KaiHe-better <393054107@qq.com>
2022-08-01 15:37:47 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-Author: Andrew
-Date: 2021-09-13 19:52:18
-LastEditors: Andrew
-LastEditTime: 2021-09-13 20:08:47
-FilePath: /BART-pretrain/models/__init__.py
-'''
-from .model import BartForFewShotLearning
